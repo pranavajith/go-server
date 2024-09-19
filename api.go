50759ce2a68c66250a51a8dfa7a4321770d68e5a
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-19 09:56:32 +0530

    sg

--- api.go
+++ api.go
@@ -1,9 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			writeJSON(w, http.StatusBadRequest, nil)
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
+}
+
+type APIError struct {
+	Error string
 }
 
 func newAPIServer(ListenAddr string) *APIServer {
@@ -12,6 +39,34 @@ func newAPIServer(ListenAddr string) *APIServer {
 	}
 }
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	log.Println("JSON API server running on port: ", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+}
+
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("method not allowed. Requested method: %s", r.Method)
+}
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleTransfers(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	server := newAPIServer(":8000")
+	server.Run()
 	fmt.Println("Heyo!")
 }
